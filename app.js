aaad7a6ff4d33a94f2748fa0c7b177a9d77826fe
andre <andrebeet1@gmail.com>
2025-06-02 12:03:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,17 +10,16 @@ app.use(express.static("public"));
 
 // API pour obtenir les notes spirituelles
 app.get("/api/notes", async (req, res) => {
-try {
-const notes = await generateNotes();
-res.status(200).send(notes);
-} catch (error) {
-console.error("Erreur lors de la génération des notes :", error.message);
-res.status(500).send("Erreur interne du serveur.");
-}
+  try {
+    const notes = await generateNotes();
+    res.status(200).send(notes);
+  } catch (error) {
+    console.error("Erreur lors de la génération des notes :", error.message);
+    res.status(500).send("Erreur interne du serveur.");
+  }
 });
 
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
-console.log(✅ Serveur lancé sur "http://localhost:" + PORT);
-);
+app.listen(PORT, () => {
+  console.log(`✅ Serveur lancé sur http://localhost:${PORT}`);
+});
